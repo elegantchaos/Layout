bc64454cbd24cbfa4175725598ba93b5f02ab6e4
Sam Deane <sam@elegantchaos.com>
2020-01-21 14:20:29 +0000

    Renamed package as LayoutExtensions for consistency with my other extension packages.

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Layout",
+    name: "LayoutExtensions",
     platforms: [
         .macOS(.v10_12), .iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Layout",
-            targets: ["Layout"]),
+            name: "LayoutExtensions",
+            targets: ["LayoutExtensions"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Layout",
+            name: "LayoutExtensions",
             dependencies: []),
         .testTarget(
-            name: "LayoutTests",
-            dependencies: ["Layout"]),
+            name: "LayoutExtensionsTests",
+            dependencies: ["LayoutExtensions"]),
     ]
 )
